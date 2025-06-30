e65e2483a8cede268854ef4f060c597419b2aad8
peswaran <parv08@gmail.com>
2025-06-30 13:01:19 -0400

    last test

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "<html><body><h1>Test aNew workflow Application</h1></body></html>\n"
+    return "<html><body><h1>confirming deployment works</h1></body></html>\n"
